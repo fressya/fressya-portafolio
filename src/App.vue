0273c240f34c07d75ecf49bda889674e8e548679
Lucia <fresya1@live.com>
2024-02-27 16:54:42 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <TheMenu>
+    </TheMenu>
+    <div class="overlay">
+        </div>
+    <router-view />
+
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+<script lang="ts" setup>
+import TheMenu from './components/home/TheMenu.vue';
 
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-});
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@font-face {
+  font-family: "Roboto";
+  src: url("./fonts/Roboto/Roboto-Bold.ttf");
+  font-weight: 700;
+  font-display: swap;
+  font-style: normal;
 }
+@font-face {
+  font-family: "Roboto";
+  src: url("./fonts/Roboto/Roboto-Regular.ttf");
+  font-weight: 400;
+  font-display: swap;
+  font-style: normal;
+}
+@font-face {
+  font-family: "Roboto";
+  src: url("./fonts/Roboto/Roboto-Light.ttf");
+  font-weight: 200;
+  font-display: swap;
+  font-style: normal;
+}
+body {
+    margin: 0;
+    padding: 0;
+}
+.overlay {
+    position: absolute;
+    top: 13%;
+    left: 20%;
+    transform: translate(-50%, -50%);
+    width: 45%;
+    height: 174%;
+    background-color: rgba(127, 41, 53, 0.3);
+    -webkit-backdrop-filter: blur(10px);
+    backdrop-filter: blur(10px);
+    z-index: -1;
+}
+
+
 </style>
